d12d22726597dcf7db112f507e799fdf7b136f75
pranoti-g <70886873+pranoti-g@users.noreply.github.com>
2022-01-04 15:56:27 +0530

    Update home.service.ts

--- src/app/service/home.service.ts
+++ src/app/service/home.service.ts
@@ -13,14 +13,14 @@ export class HomeService {
 
   getAllTrade():Observable<any>
   {
-    this.url="https://fast-trade.azurewebsites.net/home";
+    this.url="https://fast-trade-app.azurewebsites.net/home";
     //this.url="http://localhost:8080/home";
     return this.http.get(this.url);
   }
 
   getDetails(symbol:string):Observable<any>
   {
-    this.url="https://fast-trade.azurewebsites.net/details?symbol="+symbol;
+    this.url="https://fast-trade-app.azurewebsites.net/details?symbol="+symbol;
     //this.url="http://localhost:8080/details?symbol="+symbol;
     return this.http.get(this.url);
   }
